e00c0b2741a43b5fc348aa26c028ad4ba6a9e92c
René Tinz <rt653@cam.ac.uk>
2026-07-18 18:42:07 +0100

    added config

--- updated_model/prototypes/model_versions_config.py
+++ updated_model/prototypes/model_versions_config.py
@@ -23,4 +23,16 @@ model_versions = {
         maximum_fast_weight=1.0, maximum_slow_weight=0.2,
         fast_weight_blend=1.0, slow_weight_blend=1.0,
     ),
+    '2cpr_Wslow_cap_high': ModelParameters(
+        number_of_conjunction_units=number_of_conjunction_units,
+        num_cues_per_rule=num_cues_per_rule,
+        conjunction_lateral_weight=-0.45, conjunction_self_weight=1.0,
+        conjunction_to_feature_gain=0.08,
+        feature_lateral_weight=-0.28, feature_self_weight=0.73,
+        feature_to_conjunction_gain=0.04,
+        baseline_activity=0.175,
+        fast_learning_rate=0.02, slow_learning_rate=0.0002,
+        maximum_fast_weight=1.0, maximum_slow_weight=0.7,
+        fast_weight_blend=1.0, slow_weight_blend=1.0,
+    )
 }
